c90fcb37662b432ce7732686aa9a4c7a847037a1
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-01-29 17:16:18 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [selectedProducts,setSelected]=useState([]);
   const [total,setTotal]=useState(0);
   const [totalPrice,setTotalPrice]=useState(0);
+  const [category,setCategory]=useState("all")
 
   useEffect(()=>{
     axios.get('https://fakestoreapi.com/products').then((res)=>{
@@ -25,13 +26,28 @@ function App() {
     <AppContext.Provider value={{selectedProducts,setSelected,totalPrice,setTotalPrice,total,setTotal}}>
     <div className="App">
       <Header/>
-      <div className='num'>{length} PRODUCT FOUND</div>
+      <div className='num row'>
+        <span className='col-sm-6'>
+          {
+            category !=='all'?products.filter(product=>product.category===category).length:products.length
+          } PRODUCT FOUND
+        </span>
+        <div className="list-group col-md-6">
+          <button type="button" className={`list-group-item list-group-item-action ${category==="all"?"active":""}`} onClick={()=>setCategory('all')}>All</button>
+          <button type="button" className={`list-group-item list-group-item-action ${category==="men's clothing"?"active":""}`} onClick={()=>setCategory("men's clothing")}>men's clothing</button>
+          <button type="button" className={`list-group-item list-group-item-action ${category==="women's clothing"?"active":""}`} onClick={()=>setCategory("women's clothing")}>women's clothing</button>
+          <button type="button" className={`list-group-item list-group-item-action ${category==="jewelery"?"active":""}`} onClick={()=>setCategory('jewelery')}>jewelery</button>
+          <button type="button" className={`list-group-item list-group-item-action ${category==="electronics"?"active":""}`} onClick={()=>setCategory('electronics')}>electronics</button>
+        </div>
+          </div>
       <div className='products row m-4'>
         {
         products.map(product => {
+          if(category==='all'|| product.category===category){
             return <Product props={product} key={product.id}/>
-          })
-        }
+          }
+          return ''
+        })}
       </div>
     </div>
     </AppContext.Provider>
